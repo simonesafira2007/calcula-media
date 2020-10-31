62e3d30e4fa752a9a55c179b9e12381a5d373ed9
simonesafira2007 <simone.safira2007@gmail.com>
2020-10-31 15:20:17 -0300

    correção

--- imparPar.js
+++ imparPar.js
@@ -6,7 +6,7 @@ inteiro é PAR ou ÍMPAR.
 */
 const number = input.question("Entre com um numero:");
 
-if (number % 2 == 0) { // mod 2  - usa-se % em Javascript
+if (number % 2 == 0) { // mod 2  - use  % em Javascript
   console.log("PAR");
 } else {
   console.log("ÍMPAR");
