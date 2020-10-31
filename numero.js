a55aa7f86d376964a573f2446ff9a2493714e8c0
simonesafira2007 <simone.safira2007@gmail.com>
2020-10-31 08:23:23 -0300

    changes

--- numero.js
+++ numero.js
@@ -3,13 +3,13 @@ const input = require('readline-sync')
 /* Elabore um programinha que leia um número, apresente a raiz quadrada e o
 valor deste número elevado ao quadrado.
 */
-
+   
 let numero = input.question('digite um numero :')
 raizQuadrada = numero * numero
 valorRaizQuadrada = raizQuadrada * raizQuadrada
 
 
 console.log(`A raiz quadrada do número informado pelo usuário foi : ${raizQuadrada}`)
-console.log(`A raiz quadrada da raiz quadrada obtida foi : ${valorRaizQuadrada}`)
+console.log(`O quadrado de ${raizQuadrada}  foi : ${valorRaizQuadrada}`)
 
 
