6346b1d6dfaf9138f9f8f469eb54d7bc8c52e940
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-29 08:25:08 -0300

    acréscimos

--- exercicio1.js
+++ exercicio1.js
@@ -1,3 +1,5 @@
+//Exemplo do comando de repetição WHILE
+
 let n = 0;
 let x = 0;
 while (n < 5) {
@@ -14,3 +16,27 @@ while (n < 5) {
 15
 
 */  
+
+
+console.log("_________________________________")
+console.log("_________________________________")
+
+//Exemplo do comando de repetição DO WHILE
+
+i = 0;
+do { 
+    i = i+ 1;
+    console.log(i)
+
+} while (i <= 5)
+
+/*
+1
+2
+3
+4
+5
+6
+
+*/
+
