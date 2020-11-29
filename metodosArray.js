76396ea2ce93cbf9ded3baf358951b1f4248323e
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-29 12:20:08 -0300

    complemento

--- metodosArray.js
+++ metodosArray.js
@@ -37,4 +37,24 @@ console.log(numeros)
 
 console.log("  *************************  ")
 
+/*
+  **** MÉTODOS DO ARRAY ***  
+ ___________________________
+Imprimindo o array de meses do ano
+[ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho' ]
+  *************************
+adicionando um elemento (string = Agosto) ao final do array usando o push
+[ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Agosto' ]
+  *************************
+removendo o novo elemento (string = Agosto) do final do array usando o pop
+[ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho' ]
+Imprindo o array de meses do ano
+[ 20, 57, 81, 67, 2, 10 ]
+adiciona um ou mais  elementos (8, 15, 92) no começo do array usando o unshift
+[
+   8, 15, 92, 20, 57,
+  81, 67,  2, 10
+]
+  *************************
 
+*/
